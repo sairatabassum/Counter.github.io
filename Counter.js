4a828bbd6af7a884a76a03580f924cefc33195fe
Tabassum <sairatabassum386@gmail.com>
2021-05-21 19:31:32 +0600

    local storage

--- Counter.js
+++ Counter.js
@@ -1,22 +1,47 @@
 
-var cnt = 0;
 var count = document.querySelector('h1');
 
 var inc = document.querySelector(".inc");
 
 var dec = document.querySelector(".dec");
 
+count.innerHTML = localStorage.clickcount;
+
 
 
 inc.onclick = function () {
-cnt++;
-count.innerHTML=cnt;
 
+
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        }
+        else {
+            localStorage.clickcount = 1;
+        }
+        count.innerHTML = localStorage.clickcount;
+    }
+    else {
+        count.innerHTML = "Your browser does not support web storage";
+
+    }
 }
 
 dec.onclick = function () {
 
-cnt--;
-count.innerHTML=cnt;
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) - 1;
+
+        }
+        else {
+            localStorage.clickcount = 1;
+        }
+        count.innerHTML = localStorage.clickcount;
+    }
+    else {
+        count.innerHTML = "Your browser does not support web storage";
+
+    }
 }
 
